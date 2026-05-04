0a641b55db8552dec80d453e017d96b80cd64fb4
KIITIAN <2305499@kiit.ac.in>
2026-05-04 20:46:09 +0530

    changes in json file

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://campus-issue-tracker-4.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;
